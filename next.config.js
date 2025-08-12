6921eb7946adeb3b271ec934dfeedbf4ca3977e1
Tuan0105 <61956104+Tuan0105@users.noreply.github.com>
2025-08-12 12:11:51 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-
+const repo = 'Market' // <-- THAY BẰNG TÊN REPO CỦA BẠN
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
 
 const nextConfig = {
-
+  output: 'export', // <-- Quan trọng: Bật tính năng xuất tĩnh
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
